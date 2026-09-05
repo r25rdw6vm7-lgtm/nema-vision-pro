d980c1e75ab19f534910d9b74b5d5b212c8c7145
r25rdw6vm7-lgtm <r25rdw6vm7@privaterelay.appleid.com>
2026-09-05 10:38:08 +0300

    feat(agro-met): expose stable core public API

---
+++ agro-met/core/index.ts
@@ -0,0 +1,6 @@
+export * from './agro-core';
+export * from './contracts';
+export * from './adapters';
+export * from './decision-engine';
+export * from './runtime';
+export * from './validation';
